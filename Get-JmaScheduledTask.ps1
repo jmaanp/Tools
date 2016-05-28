fa2db7883b31e872b51dd17018280e328af7321b
Anders Præstegaard <apraestegaard@gmail.com>
2016-05-28 08:50:20 +0200

    Årl najd loong

--- Get-JmaScheduledTask.ps1
+++ Get-JmaScheduledTask.ps1
@@ -1,35 +1,35 @@
 ﻿function Get-JmaScheduledTask
 {
     <#
-            .SYNOPSIS
-            Gets the task definition object of a scheduled task that is registered on the local or a remote computer.
+        .SYNOPSIS
+        Gets the task definition object of a scheduled task that is registered on the local or a remote computer.
 
-            .DESCRIPTION
-            The Get-JmaScheduledTask Cmdlet gets the task definition object of a scheduled task that is registered on the local or a remote computer utilizing the windows executable schtasks.exe.
-            Mimicing the Microsoft TaskScheduler module you can filter on the taskname with the asterix char (*).
+        .DESCRIPTION
+        The Get-JmaScheduledTask Cmdlet gets the task definition object of a scheduled task that is registered on the local or a remote computer utilizing the windows executable schtasks.exe.
+        Mimicing the Microsoft TaskScheduler module you can filter on the taskname with the asterix char (*).
 
-            .PARAMETER ComputerName
-			An array of one or more computernames.
+        .PARAMETER ComputerName
+		An array of one or more computernames.
 
-            .PARAMETER TaskName
-            Specify a path for scheduled tasks in the Task Scheduler namespace.
-            Mimicing the (handy but unfortunately only against Windows Server 2012+) Microsoft TaskScheduler module, you can filter the taskname with the asterix char (*)
+        .PARAMETER TaskName
+        Specify a path for scheduled tasks in the Task Scheduler namespace.
+        Mimicing the (handy but unfortunately only against Windows Server 2012+) Microsoft TaskScheduler module, you can filter the taskname with the asterix char (*)
 
-            .EXAMPLE
-            Get all tasks from the local computer .  
-            Get-JmaScheduledTask
+        .EXAMPLE
+        Get all tasks from the local computer .  
+        Get-JmaScheduledTask
 
-            .EXAMPLE
-            Get specific task from remote computers Server1 and server2. 
-            Get-JmaScheduledTask -TaskName '\MyFolder\MyTask' -ComputerName Server1, Server2 -Verbose
+        .EXAMPLE
+        Get specific task from remote computers Server1 and server2. 
+        Get-JmaScheduledTask -TaskName '\MyFolder\MyTask' -ComputerName Server1, Server2 -Verbose
 
-            .EXAMPLE
-            Search for task(s) from remote computers Server1 and server2 (wildcard).
-            Get-JmaScheduledTask -TaskName '\MyFolder\*' -ComputerName Server1, Server2
+        .EXAMPLE
+        Search for task(s) from remote computers Server1 and server2 (wildcard).
+        Get-JmaScheduledTask -TaskName '\MyFolder\*' -ComputerName Server1, Server2
 
-            .NOTES
-            Created by Anders Præstegaard (@aPowerShell).
-            19-05-2016	Version 1.0
+        .NOTES
+        Created by Anders Præstegaard (@aPowerShell).
+        19-05-2016	Version 1.0
     #>
 	[CmdletBinding()]
 	param
